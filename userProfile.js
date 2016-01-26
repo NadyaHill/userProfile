f7315e520fd3747e4ccad0ce3a0d30e05dcdd6aa
Student <student@RU-021.local>
2016-01-25 17:39:12 -0700

    takeThree

---
+++ userProfile.js
@@ -0,0 +1,21 @@
+angular.model("gromitMod", []);
+
+angular
+	.module("gromitMod")
+		.controller("profile", [
+			"$scope",
+			function($scope) {
+
+				$scope.jobs =
+
+				$scope.addJobs = function () {
+
+				}
+
+
+
+	
+
+
+
+	}])
